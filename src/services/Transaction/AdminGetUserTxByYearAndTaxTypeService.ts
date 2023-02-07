29ce26d625d38e0834bd3dac5374b36f4a40e890
Felipe-CdS <felipe32santos@hotmail.com>
2023-02-07 12:46:47 -0300

    GetUserYearTxs range bug fix

--- src/services/Transaction/AdminGetUserTxByYearAndTaxTypeService.ts
+++ src/services/Transaction/AdminGetUserTxByYearAndTaxTypeService.ts
@@ -1,5 +1,4 @@
-import { getCustomRepository, LessThan, MoreThan } from "typeorm"
-import { Transaction } from "../../entities/Transaction";
+import { Between, getCustomRepository, LessThan, MoreThan } from "typeorm";
 import { TransactionRepositories } from "../../repositories/TransactionRepositories";
 import { UserRepositories } from "../../repositories/UserRepositories";
 
@@ -17,9 +16,8 @@ class AdminGetUserTxByYearAndTaxTypeService {
         var transactions_search = await transactionRepository.find({
 				where: { 
 					fk_user_id: user_id,
-					transaction_date:  MoreThan(`${year}-01-01  00:00:00.00000`)
-				},
-				take: 12
+					transaction_date:  Between(`${year}-01-01  00:00:00.00000`, `${year}-12-31  23:59:59.99999`)
+				}
 		});
 
 		for(let i = (transactions_search.length -1); i >= 0; i--)
@@ -32,17 +30,8 @@ class AdminGetUserTxByYearAndTaxTypeService {
 
 		var returnArray = {};
 
-		var mostRecentEntry = await transactionRepository.findOne({
-			where: { fk_user_id: user_id },
-			order: { transaction_date: 'DESC' }
-		});
-
 		for(let i = 1; i < 13; i++)
 			returnArray[`month_${i}`] = {pending: true, blocked: true};
-
-		
-		// if(year == new Date(mostRecentEntry.transaction_date).getFullYear()))
-
 
 		(transactions_search).map((elem) => {
 			let holder = {
@@ -56,33 +45,8 @@ class AdminGetUserTxByYearAndTaxTypeService {
 					delete holder.transaction_id;
 					
 					let month_number = new Date(holder.transaction_date).getMonth();
-		
-					if(holder.transaction_date < (new Date(`${year}-12-31  23:59:59.0000`)))
-						returnArray[`month_${month_number + 1}`] = holder;
+					returnArray[`month_${month_number + 1}`] = holder;
 		})
-
-
-		// for(let i = 0; i < 12; i++){
-		// 	if(transactions_search[i])
-		// 	{
-		// 		let holder = {
-		// 			...transactions_search[i],
-		// 			tax_identifier: transactions_search[i].fk_tax_id.number_identifier,
-		// 			username:		transactions_search[i].fk_user_id.username
-		// 		};
-	
-		// 		delete holder.fk_tax_id;
-		// 		delete holder.fk_user_id;
-		// 		delete holder.transaction_id;
-				
-		// 		let month_number = new Date(holder.transaction_date).getMonth();
-	
-		// 		if(holder.transaction_date < (new Date(`${year}-12-31  23:59:59.0000`)))
-		// 			returnArray[`month_${month_number + 1}`] = holder;
-		// 	}
-		// 	else
-		// 		returnArray[`month_${i + 1}`] = {pending: true, blocked: true};
-		// }	
 
         return (returnArray);
     }
